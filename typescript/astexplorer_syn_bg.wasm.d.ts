dba55779394b91bfa61e7a819531a2cf27a3ca8a
Jordan Last <jordan.michael.last@gmail.com>
2022-03-21 10:11:58 -0500

    add generated files

---
+++ typescript/astexplorer_syn_bg.wasm.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function parseFile(a: number, b: number): number;
+export function parseDeriveInput(a: number, b: number): number;
+export function printAst(a: number, b: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
